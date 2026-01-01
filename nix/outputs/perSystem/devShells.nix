6d11a6e806c5cd11d2a48dd80be0acf02c460f70
Albert O'Shea <albertoshea2@gmail.com>
2026-01-02 01:05:17 +1100

    refactor: use devShells.d/ instead of shell-packages.d/
    - Move devShells/lintfra.nix to devShells.d/10-lintfra.nix for auto-merge
    - Remove shell-packages.d/ (non-standard naming)
    - Remove shellHook.d/ (bundled in devShell)
    - Consumers use inputsFrom = [ self'.devShells.lintfra ]

--- nix/outputs/perSystem/devShells.nix
+++ nix/outputs/perSystem/devShells.nix
@@ -1,16 +1,10 @@
-# Lintfra devshell using fragment composition
-{ pkgs, self', imp, ... }:
-let
-  shellHookFragments = imp.fragments ./shellHook.d;
-  # shell-packages.d/ contains list fragments for devshell packages
-  # (separate from packages.d/ which contains attrset fragments for self'.packages)
-  shellPackageFragments = imp.fragmentsWith { inherit pkgs self'; } ./shell-packages.d;
-in
+# Lintfra devshell - uses inputsFrom to compose with lintfra shell
+{ pkgs, self', ... }:
 {
   default = pkgs.mkShell {
-    packages = shellPackageFragments.asList ++ [ self'.formatter ];
+    inputsFrom = [ self'.devShells.lintfra ];
+    packages = [ self'.formatter ];
     shellHook = ''
-      ${shellHookFragments.asString}
       echo ""
       echo "lintfra devshell"
     '';
