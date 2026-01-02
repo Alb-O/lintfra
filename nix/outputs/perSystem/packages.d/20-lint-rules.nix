3bdcb6c1e2ca629eee4530437d588c6080f65e93
Albert O'Shea <albertoshea2@gmail.com>
2026-01-03 01:28:06 +1100

    feat: define ast-grep rules in nix, generate yaml
    Rules are now defined in lint/rules/*.nix and converted to YAML via:
    - nix eval --json | yq -P
    
    Benefits:
    - Type-safe rule definitions with mkRule helper
    - Composable with imp.gits for rule sharing across repos
    - Generated YAML is gitignored
    
    New packages:
    - lint-rules: derivation containing generated YAML files
    - lint-rules-sync: script to sync generated rules to lint/ast-rules/

---
+++ nix/outputs/perSystem/packages.d/20-lint-rules.nix
@@ -0,0 +1,39 @@
+/**
+  Package to generate ast-grep YAML rules from Nix definitions.
+*/
+{ pkgs, lib, ... }:
+let
+  astGrep = import ../../lib/ast-grep-rule.nix { inherit lib; };
+  rulesDir = ../../../../lint/rules;
+
+  # Load all .nix files from lint/rules/
+  ruleFiles = builtins.filter (f: lib.hasSuffix ".nix" f) (
+    builtins.attrNames (builtins.readDir rulesDir)
+  );
+
+  # Import each rule file
+  rules = map (f: {
+    name = lib.removeSuffix ".nix" f;
+    rule = import (rulesDir + "/${f}") { inherit (astGrep) mkRule; };
+  }) ruleFiles;
+
+  # Generate YAML files using yq to convert JSON to YAML
+  generatedRules = pkgs.runCommand "ast-grep-rules" { buildInputs = [ pkgs.yq-go ]; } ''
+    mkdir -p $out
+    ${lib.concatMapStringsSep "\n" (
+      r: ''echo '${astGrep.toJson r.rule}' | yq -P > $out/${r.name}.yml''
+    ) rules}
+  '';
+in
+{
+  lint-rules = generatedRules;
+
+  lint-rules-sync = pkgs.writeShellScriptBin "lint-rules-sync" ''
+    set -e
+    dest="''${1:-lint/ast-rules}"
+    mkdir -p "$dest"
+    rm -f "$dest"/*.yml
+    cp ${generatedRules}/*.yml "$dest/"
+    echo "Synced ${toString (builtins.length rules)} rules to $dest"
+  '';
+}
